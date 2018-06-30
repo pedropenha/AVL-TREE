5ad9545126dfd60632a8f71e042f2bbbd55c79c7
Pedro Penha <pedro.penha.martins@gmail.com>
2018-06-29 22:43:19 -0400

    Inserindo funções aloca, pre_ordem, pos_ordem, em_ordem, busca

--- avl.c
+++ avl.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define TAM 50
 
-struct avl{
-    int n, fb;
-
-    struct avl *esquerda;
-    struct avl *direita;
-    struct avl *pai;
+struct No{
+    int dado;
+    int bal;
+    struct No *esquerda;
+    struct No *direita;
+    struct No *pai;
 };
 
-struct avl *raiz = NULL;
+struct No *raiz = NULL;
+void pre_ordem(struct No *ptr);
+
 
 void menu();
 void inserir();
-void remover();
-void buscar();
-void ordem();
-void pre_ordem();
-void pos_ordem();
-void predecessor();
-void sucessor();
+struct No* Aloca(int n);
+void pos_ordem(struct No *ptr);
+void ordem(struct No *ptr);
+struct No* buscar(struct No *ptr, int n);
+struct No* minimo(struct No *ptr);
+struct No* maximo(struct No *ptr);
+struct No* sucessor(struct No *ptr);
+
 
 int main(){
     menu();
@@ -76,5 +80,83 @@ void menu(){
 }
 
 void inserir(){
-    puts("Inserido");
+
+}
+
+struct No* Aloca(int n){
+    struct No *elemento;
+
+    elemento->dado = n;
+    elemento->pai = NULL;
+    elemento->esquerda = NULL;
+    elemento->direita = NULL;
+
+    return elemento;
+}
+
+void pre_ordem(struct No *ptr){
+    if(ptr != NULL){
+        printf("%d", ptr->dado);
+        pre_ordem(ptr->esquerda);
+        pre_ordem(ptr->direita);
+    }
+}
+
+void pos_ordem(struct No *ptr){
+    if(ptr != NULL){
+        pos_ordem(ptr->esquerda);
+        pos_ordem(ptr->direita);
+        printf("%d", ptr->dado);
+    }
+}
+
+void ordem(struct No *ptr){
+    if(ptr != NULL){
+        ordem(ptr->esquerda);
+        printf("%d", ptr->dado);
+        ordem(ptr->direita);
+    }
+}
+
+struct No* buscar(struct No *ptr, int n){
+    if(ptr == NULL || n == ptr->dado)
+        return ptr;
+    if(n < ptr->dado)
+        return buscar(ptr->esquerda, n);
+    else
+        return buscar(ptr->direita, n);
+}
+
+struct No* minimo(struct No *ptr){
+
+    while (ptr->esquerda != NULL){
+        ptr = ptr->esquerda;
+    }
+
+    return ptr;
+}
+
+struct No* maximo(struct No *ptr){
+
+    while (ptr->direita != NULL){
+        ptr = ptr->direita;
+    }
+
+    return ptr;
+}
+
+struct No* sucessor(struct No *ptr){
+    struct No *aux;
+
+    if(ptr->direita != NULL)
+        return minimo(ptr->direita);
+
+    aux = ptr->pai;
+
+    while (aux != NULL && ptr == aux->direita){
+        ptr = aux;
+        aux = aux->pai;
+    }
+
+    return aux;
 }
